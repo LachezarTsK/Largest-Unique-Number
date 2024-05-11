17724372a9707e46b7d1114d91259dbfec248f18
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-05-11 22:28:51 +0200

    Update Solution.kt

--- Solution.kt
+++ Solution.kt
@@ -3,21 +3,21 @@ class Solution {
 
     private companion object {
         val RANGE_OF_VALUES = intArrayOf(0, 1000)
-        const val NO_UNIQUE_INT_FOUND = -1;
+        const val NO_UNIQUE_INT_FOUND = -1
     }
 
     fun largestUniqueNumber(input: IntArray): Int {
-        val frequency = IntArray(RANGE_OF_VALUES[1] + 1);
+        val frequency = IntArray(RANGE_OF_VALUES[1] + 1)
         for (value in input) {
-            ++frequency[value];
+            ++frequency[value]
         }
 
         for (value in RANGE_OF_VALUES[1] downTo RANGE_OF_VALUES[0]) {
             if (frequency[value] == 1) {
-                return value;
+                return value
             }
         }
 
-        return NO_UNIQUE_INT_FOUND;
+        return NO_UNIQUE_INT_FOUND
     }
 }
