399c22215a4365e83bbebe3c8ceaa84bd79e1e28
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-05-11 22:22:50 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,22 @@
+
+/**
+ * @param {number[]} input
+ * @return {number}
+ */
+var largestUniqueNumber = function (input) {
+    const RANGE_OF_VALUES = [0, 1000];
+    const NO_UNIQUE_INT_FOUND = -1;
+
+    const frequency = new Array(RANGE_OF_VALUES[1] + 1).fill(0);
+    for (let value of input) {
+        ++frequency[value];
+    }
+
+    for (let value = RANGE_OF_VALUES[1]; value >= RANGE_OF_VALUES[0]; --value) {
+        if (frequency[value] === 1) {
+            return value;
+        }
+    }
+
+    return NO_UNIQUE_INT_FOUND;
+};
